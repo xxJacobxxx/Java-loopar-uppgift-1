43e897c7e3c12728bf0b54b5c846484310bf064a
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-11-08 10:34:26 +0100

    g delen klar

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        for(int i=5;i>-1;i--){
-            System.out.println(i);
+        int fält[]=new int[5];
+        Scanner in = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            System.out.println("Skriv in tal nr "+i);
+            fält[i]=in.nextInt();
+
+        }
+        System.out.println("Här kommer talen");
+        for(int i=0; i<5;i++){
+            System.out.println(fält[i]);
         }
     }
 }
