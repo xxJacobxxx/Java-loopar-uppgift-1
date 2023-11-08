741eee87ae691dc0ced53834120c2e6d49f5ff08
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-11-08 10:03:53 +0100

    Andra delen klar (b delen)

--- src/App.java
+++ src/App.java
@@ -1,9 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int tal=0;
+        int tal=2;
         while(tal<=5){
             System.out.println(tal);
             tal++;
         }
+
     }
 }
