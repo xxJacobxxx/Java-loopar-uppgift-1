65e0bb5f736f6699017309221b10d253d50d2173
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-11-08 10:16:13 +0100

    f delen klar

--- src/App.java
+++ src/App.java
@@ -1,12 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int i =0;
-        int varv=6;
-        while(varv>0){
+        for(int i=5;i>-1;i--){
             System.out.println(i);
-            varv=varv-1;
-            i++;
         }
-
     }
 }
