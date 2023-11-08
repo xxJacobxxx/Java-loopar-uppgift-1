d325d024510e9cdfe67cc91cbb011eefa2f2952a
Jacob <jacob.hansen@elev.ga.ntig.se>
2023-11-08 10:03:05 +0100

    Första delen klar

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int tal=0;
+        while(tal<=5){
+            System.out.println(tal);
+            tal++;
+        }
+    }
+}
